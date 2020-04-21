d93a50e508de0c5b7206dccd548fab446ba48d05
unknown <peek.zeeshan@gmail.com>
2020-04-22 00:31:23 +0500

    nmber setting

--- proposals/post-gig.php
+++ proposals/post-gig.php
@@ -958,7 +958,16 @@ if(isset($_POST['publish'])){
         var custom_btn = $('.input-number').val();
         $('#days30').val(custom_btn);
       });
+       $(".input-number").keypress(function (e) {
+        //if the letter is not digit then display error and don't type anything
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+           //display error message
+           $("#errmsg").html("Digits Only").show().fadeOut("slow");
+                  return false;
+       }
+      });
 
+      
     });
 
       $(function(){
